e8c5a8a14df6888e2765a57981da7159de7dbcf5
Hannah Kania <82820043+hkania@users.noreply.github.com>
2023-11-15 16:51:21 -0500

    Update earlgrey_lem_cat.sh

--- earlgrey_lem_cat.sh
+++ earlgrey_lem_cat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --mem-per-cpu=4G  # adjust as needed
-#SBATCH -c 8 # number of threads per process
+#SBATCH -c 16 # number of threads per process
 #SBATCH --output=log/###.out #rename the ### for your genome
 #SBATCH --error=log/###.err
 #SBATCH --partition=scavenger
@@ -12,7 +12,7 @@
 source /hpc/home/hpk4/miniconda3/etc/profile.d/conda.sh #change to your conda (should be /hpc/home/netID/miniconda3/etc/profile.d/conda.sh)
 conda activate earlgrey
 # Run earlGrey
-earlGrey -g ncbi_dataset/data/GCA_020740605.1/GCA_020740605.1_mLemCat1.pri_genomic.fna -s lemurCatta -o earlGrey -t 8
+earlGrey -g ncbi_dataset/data/GCA_020740605.1/GCA_020740605.1_mLemCat1.pri_genomic.fna -s lemurCatta -o earlGrey -t 16
 
 # for the flags, -s is just the name of the output files, so for whichever species you do type that, -o is the output directory earlgrey will make for you
 # -g is the genome, so use the same oath that you used for RepeatMasker
